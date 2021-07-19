5d1a248710811d7c7fa9ce46bbbcc5a68981ff45
Kevin Herrera <kevin@herrera.io>
2021-07-19 13:22:43 +0000

    Adding a constant for the managed label name.

--- dev/__init__.py
+++ dev/__init__.py
@@ -1,3 +1,7 @@
 from typing import Final
 
+# The version of "dev".
 __version__: Final[str] = '0.1.0'
+
+# The name of the managed label.
+LABEL_NAME: Final[str] = 'io.github.kherge.py-dev.managed'
